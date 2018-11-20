f6ba20c06a016434e3e63f7ff21579664f870736
fahrulrizal <hellorizal4@gmail.com>
2018-11-20 23:34:56 +0800

    first commit

---
+++ resources/views/template/app.blade.php
@@ -0,0 +1,14 @@
+@include('template.header')
+<div class="container-fluid">
+	<div class="row" style="position: relative; top: 100px;">
+	<div class="col-md-3">
+		<div class="card" style="width: 18rem;">
+				<img class="card-img-top" src="img/2.jpg" alt="Card image cap">
+				<div class="card-body">
+				<h5 class="card-title">Card title</h5>
+				<p>Lorem Ipsum</p>
+				<button class="btn btn-xs btn-primary">Read More</button>
+		</div>
+	</div>
+</div>
+@include('template.footer')
---
+++ resources/views/web/child/home.blade.php
@@ -0,0 +1,104 @@
+@extends('web.layout.app')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-3">
+        
+        <div class="card">
+            <img class="card-img-top" src="img/2.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cacontent.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        </div>
+         <div class="col-md-3">
+        
+        <div class="card">
+            <img class="card-img-top" src="img/2.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cacontent.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        </div>
+        <div class="col-md-3">
+        
+        <div class="card">
+            <img class="card-img-top" src="img/2.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cacontent.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        </div>
+        <div class="col-md-3">
+        
+        <div class="card">
+            <img class="card-img-top" src="img/2.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cacontent.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        </div>
+        <div class="col-md-3">
+        
+        <div class="card">
+            <img class="card-img-top" src="img/2.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cacontent.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        </div>
+        <div class="col-md-3">
+        
+        <div class="card">
+            <img class="card-img-top" src="img/2.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cacontent.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        </div>
+        <div class="col-md-3">
+        
+        <div class="card">
+            <img class="card-img-top" src="img/2.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cacontent.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        </div>
+
+         <div class="col-md-3">
+        
+        <div class="card">
+            <img class="card-img-top" src="img/2.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cacontent.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        </div>
+        
+        
+    </div>
+</div>
+<style>
+    .card{
+        margin-top:20px;
+    }
+</style>
+@endsection
